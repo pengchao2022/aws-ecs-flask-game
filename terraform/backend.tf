a13b334143cef85c4deb9923021578fa5645f816
pengchao <pengchao.ma2@outlook.com>
2025-09-24 18:38:18 +0800

    modified

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraformstatefile090909"
-    key            = "aws-ecs-flask-app.tfstate"
+    key            = "aws-ecs-flask-todo.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
