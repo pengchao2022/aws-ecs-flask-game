8ca39c48efba5a76c179e12cac0385fa17eef818
Pengchao_git <118711195+pengchao2022@users.noreply.github.com>
2025-09-25 12:31:34 +0800

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraformstatefile090909"
-    key            = "aws-ecs-flask-games.tfstate"
+    key            = "aws-ecs-flask-games-1.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
